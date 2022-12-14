795d0539ea0b9d90117c7ab8fbe2ce0fdc72db6f
vivekkum-ar <vivekkr9119@gmail.com>
2022-12-14 15:22:00 +0530

    Removed unnecessary comments

--- content1.js
+++ content1.js
@@ -4,10 +4,8 @@
     var button = document.createElement("button");
     button.classList.add("btn","border-0","warning","d-inline-flex","background-warning","justify-content-center","align-items-center");
     button.setAttribute("type","button");
-    button.setAttribute("style","background:#F5C518!important;");
     button.id = "main_button";
     button.innerHTML = '<i class="fab pe-1 fa-chromecast text-dark"></i> <h4 class="text-dark">moviesdirect</h4>';
     main_DIV.appendChild(button);
-    // document.getElementById("home_img_button").nextElementSibling.insertBefore(main_DIV);
     document.getElementById("home_img_holder").insertAdjacentElement("afterend",main_DIV);
 })();

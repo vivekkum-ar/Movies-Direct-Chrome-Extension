5f52b94fc28c422f1b83424b8a58f98174c2d03d
vivekkum-ar <vivekkr9119@gmail.com>
2022-12-16 13:34:36 +0530

    Fetch added from OMdB API

---
+++ popup.js
@@ -0,0 +1,34 @@
+const fetchMoviesButton = document.getElementById("md-searchBtn");
+const cancelFetchButton = document.getElementById("md-searchBox");
+
+
+let controller = null;
+
+document.getElementById("md-searchBtn").addEventListener("click", async () => {
+  controller = new AbortController();
+  try {
+    log("Request started...");
+    const response = await fetch('https://www.omdbapi.com/?t='+document.getElementById("md-searchBox").value+'&apikey=YOUR_API_KEY', {
+      signal: controller.signal
+    });
+    const movies = await response.json();
+    console.log(movies); // Was previousely console.log(`Fetched movies: ${JSON.stringify(movies)}`);
+  } catch (error) {
+    log(`Fetch error: ${error.name}`);
+  }
+  controller = null;
+});
+
+cancelFetchButton.addEventListener("click", () => {
+  if (controller) {
+    controller.abort();
+  }
+});
+
+/* -------------------------------------------------------------------------- */
+/*           Temporary log function tat logs the response from fetch          */
+/* -------------------------------------------------------------------------- */
+function log(message) {
+  document.getElementById("message-new").innerText = message;
+}
+

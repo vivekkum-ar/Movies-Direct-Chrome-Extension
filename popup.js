09a2c8aa19915177b31fc9b5a124e68de115f672
vivekkum-ar <vivekkr9119@gmail.com>
2022-12-17 10:25:20 +0530

    (W)Search now returns movie

--- popup.js
+++ popup.js
@@ -13,6 +13,7 @@ document.getElementById("md-searchBtn").addEventListener("click", async () => {
     });
     const movies = await response.json();
     console.log(movies); // Was previousely console.log(`Fetched movies: ${JSON.stringify(movies)}`);
+    setTimeout(populateHscrollA(movies), 1000);
   } catch (error) {
     log(`Fetch error: ${error.name}`);
   }
@@ -33,3 +34,11 @@ function log(message) {
 }
 
 
+/* -------------------------------------------------------------------------- */
+/*          Function that populates the "horizontal-scroll-a" that is         */
+/*                      present in the hellomd.html file                      */
+/* -------------------------------------------------------------------------- */
+function populateHscrollA(movies){
+  html = '<div class="col-6 grid-item">  <div class="product-grid">      <div class="product-image">          <a href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/" class="image"><img src='+movies.Poster+'></a>          <span class="product-sale-label-right"><b>' + movies.imdbRating + ' </b><i class="fas fa-star"></i></span>          <span class="product-sale-label-left">' + movies.Rated + '</span>      </div>      <div class="product-content">          <h3 class="title"><a href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/">' + movies.Title + '</a></h3>          <div class="price">            ' + movies.Released + '          </div>          <a class="add-to-cart" href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/">            <i class="fas fa-play-circle"></i>watch now</a>      </div>  </div></div>';
+  document.querySelector('.horizontal-scroll-a').children[1].innerHTML = html;
+}

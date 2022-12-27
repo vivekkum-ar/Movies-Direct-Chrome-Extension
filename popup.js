82eaa2e330a27a87b79726ca04fa8b913e951d03
vivekkum-ar <vivekkr9119@gmail.com>
2022-12-27 23:05:20 +0530

    Added retrieve via API

--- popup.js
+++ popup.js
@@ -1,21 +1,25 @@
+/* ---------------------------------------------------------------------------------------------- */
+/*                       Initialized search button and cancel search button                       */
+/* ---------------------------------------------------------------------------------------------- */
 const fetchMoviesButton = document.getElementById("md-searchBtn");
 const cancelFetchButton = document.getElementById("md-searchBox");
 
-
+/* ---------------------------------------------------------------------------------------------- */
+/*                                  fetch for horizontal scroll a                                 */
+/* ---------------------------------------------------------------------------------------------- */
 let controller = null;
-
 document.getElementById("md-searchBtn").addEventListener("click", async () => {
   controller = new AbortController();
   try {
-    log("Request started...");
+    console.log("Request started...");
     const response = await fetch('https://www.omdbapi.com/?t='+document.getElementById("md-searchBox").value+'&apikey=YOUR_API_KEY', {
       signal: controller.signal
     });
     const movies = await response.json();
     console.log(movies); // Was previousely console.log(`Fetched movies: ${JSON.stringify(movies)}`);
-    setTimeout(populateHscrollA(movies), 1000);
+    populateHscrollA(movies);
   } catch (error) {
-    log(`Fetch error: ${error.name}`);
+    console.log(`Fetch error: ${error.name}`);
   }
   controller = null;
 });

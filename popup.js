2a026ea21593f600f1d99df1aab70645d92e6139
vivekkum-ar <vivekkr9119@gmail.com>
2022-12-30 23:28:06 +0530

    HscrollB added w/ fetch
    The css for a Horizontal scroll element was also added along with the fetch() code for fetching the you might like section with 25 movies from hosted json.

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@
 /* ---------------------------------------------------------------------------------------------- */
 const fetchMoviesButton = document.getElementById("md-searchBtn");
 const cancelFetchButton = document.getElementById("md-searchBox");
-
 /* ---------------------------------------------------------------------------------------------- */
 /*                                  fetch for horizontal scroll a                                 */
 /* ---------------------------------------------------------------------------------------------- */
@@ -45,4 +44,40 @@ function log(message) {
 function populateHscrollA(movies){
   html = '<div class="col-6 grid-item">  <div class="product-grid">      <div class="product-image">          <a href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/" class="image"><img src='+movies.Poster+'></a>          <span class="product-sale-label-right"><b>' + movies.imdbRating + ' </b><i class="fas fa-star"></i></span>          <span class="product-sale-label-left">' + movies.Rated + '</span>      </div>      <div class="product-content">          <h3 class="title"><a href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/">' + movies.Title + '</a></h3>          <div class="price">            ' + movies.Released + '          </div>          <a class="add-to-cart" href="https://v2.vidsrc.me/embed/' + movies.imdbID + '/">            <i class="fas fa-play-circle"></i>watch now</a>      </div>  </div></div>';
   document.querySelector('.horizontal-scroll-a').children[0].innerHTML = html;
+}
+
+/* -------------------------------------------------------------------------- */
+/*    Seperate other fetch for horizontal scroll b because it needs to run    */
+/*                       as soon as the page initializes                      */
+/* -------------------------------------------------------------------------- */
+let controller2 = null;
+async function fetchYouMightLikeMovies() {
+  controller2 = new AbortController();
+  try {
+    console.log("2Request started...");
+    const response2 = await fetch(you_might_like_json_link_here , {
+      signal: controller2.signal
+    });
+    const movies2 = await response2.json();
+    console.log(movies2); // Was previousely console.log(`Fetched movies: ${JSON.stringify(movies)}`);
+    populateHscrollB(movies2);  //setTimeout(populateHscrollB(movies2), 1000);
+  } catch (error) {
+    console.log(`2Fetch error: ${error.name}`);
+  }
+  controller2 = null;  //we can controller2.abort for cancelling the request. But we didnt need to.
+}
+/* --------- fetchYouMightLikeMovies function runs on page initialization --------- */
+fetchYouMightLikeMovies();
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                    Function that populates the "horizontal-scroll-b" that is                   */
+/*                                present in the hellomd.html file                                */
+/* ---------------------------------------------------------------------------------------------- */
+function populateHscrollB(movies2){
+  html2 = "";
+  for (let index = 0; index < 25; index++) {
+    // const element = movies2[index];
+    html2 += '<div class="col-6 grid-item">  <div class="product-grid">      <div class="product-image">          <a href="https://v2.vidsrc.me/embed/' + movies2[index].imdbID + '/" target="_blank" class="image"><img src='+movies2[index].Poster+'></a>          <span class="product-sale-label-right"><b>' + movies2[index].imdbRating + ' </b><i class="fas fa-star"></i></span>          <span class="product-sale-label-left">' + movies2[index].Rated + '</span>      </div>      <div class="product-content">          <h3 class="title"><a href="https://v2.vidsrc.me/embed/' + movies2[index].imdbID + '/" target="_blank">' + movies2[index].Title + '</a></h3>          <div class="price">            ' + movies2[index].Released + '          </div>          <a class="add-to-cart" href="https://v2.vidsrc.me/embed/' + movies2[index].imdbID + '/" target="_blank">            <i class="fas fa-play-circle"></i>watch now</a>      </div>  </div></div>';
+  }
+  document.querySelector('.horizontal-scroll-b').children[0].innerHTML = html2;
 }
